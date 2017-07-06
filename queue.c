ed1b79563731f10fc1c672596fc732561f700621
Aravind Vaddi <aravindvaddi.github@gmail.com>
2017-07-06 14:43:30 +0530

    Added implementations: - function to print queue - function to return length of queue

--- queue.c
+++ queue.c
@@ -10,6 +10,41 @@ int q_empty (queue *q)
 		return (!q->head) ? 1 : 0;
 }
 
+/* function to print queue */
+
+void q_print (queue *q)
+{
+	node *temp;
+	if(q)
+	{
+		temp = q->head;
+
+		while(temp)
+		{
+			printf("%d ", temp->data);
+			temp = temp->next;
+		}
+
+		printf("\n");
+	}
+}
+
+/* function to return length of queue */
+
+int q_length (queue *q)
+{
+	int i;
+	node *temp;
+
+	if(q)
+	{
+		temp = q->head;
+		for(i = 0; temp; i++, temp = temp->next);
+	}
+
+	return i;
+}
+
 /* function to initialize a queue pointer */
 
 void q_create (queue **q)
